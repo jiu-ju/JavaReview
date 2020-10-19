0ed86cac094a99ae50e1a4c8ed6122737cec30d1
jiu-ju <57570110+DzhulAleks@users.noreply.github.com>
2020-10-19 14:26:52 -0500

    enums

---
+++ src/main/java/com/review/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.review.enums;
+
+public enum Gender {
+    MALE,FEMALE;
+}
---
+++ src/main/java/com/review/enums/Status.java
@@ -0,0 +1,16 @@
+package com.review.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Status {
+
+    OPEN("Open"),
+    IN_PROGRESS("In Progress"),
+    UAT_TEST("UAT Testing"),
+    COMPLETED("Completed");
+
+    private final String value;
+}
