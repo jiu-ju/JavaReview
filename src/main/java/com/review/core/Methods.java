c33a3e5cc6437e4ce1399fc1db283ad2d806216d
jiu-ju <57570110+DzhulAleks@users.noreply.github.com>
2020-10-19 14:19:04 -0500

    core

---
+++ src/main/java/com/review/core/Methods.java
@@ -0,0 +1,24 @@
+package com.review.core;
+
+public class Methods {
+
+    public void methodA(){
+        System.out.println("methodA() is called");
+    }
+
+    public void methodA(String str){
+        System.out.println("methodA(String str) is called");
+    }
+
+    public String methodB(){
+        return "methodB() is called";
+    }
+
+    public String methodB(int x){
+        return "methodB(int x) is called";
+    }
+
+    public static void methodC(){
+        System.out.println("methodC() is called");
+    }
+}
